4fb75ce9af7f6c0967f8dbae7dfaf1ef1e27fcca
felipe2028 <phillipmarkle@gmail.com>
2025-03-29 12:22:29 -0500

    made a guessing game in hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,6 @@
-print("Guess a number between 1 and 10")
-print("You will get 3 guesses")
+names = ["John", "Dave", "Bob", "Jim"]
+keys = ["Red", "Blue", "Green", "Yellow"]
+values = [1, 2, 3, 4]
 
-guess_count = 0
-guess_limit = 3
-secret_number = 5
-
-while guess_count < guess_limit:
-    guess = input("Enter a guess: ")
-    if not guess.isnumeric():
-        print("You must enter an integer. Try again.")
-    else:
-        guess_count += 1
-        if int(guess) == secret_number:
-            print("You win!")
-            break
+a = zip(names, keys, values)
+print(list(a))
